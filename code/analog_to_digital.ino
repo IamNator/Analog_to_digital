3283da19a4309a83abf5380c6f89ca05407770f6
Nator Verinumbe <natverior1@gmail.com>
2020-01-30 02:38:59 +0100

    organising code

--- code/analog_to_digital.ino
+++ code/analog_to_digital.ino
@@ -8,31 +8,56 @@ void setup() {
   Serial.begin(9600); //sets up the baud rate for UART communication serial monitor of screen.
 }
 
+int RpmFunction();
+int TorqueFunction();
+
+
+
 void loop() {
-  // put your main code here, to run repeatedly:
-  float analog_speed_value = 0; //initialize speed value as zero
-  float Speed; 
-  float finalSpeed;
+  RpmFunction();
+  TorqueFunction();   
+}
   
-//  for(int i=0; i<150; i++){
-  analog_speed_value = analogRead(SpeedSensor); //reads speed value (range between 0 to 1024)
- // }
-  delay(10);
-  //Speed_value = Speed_value/150;
- 
-  finalSpeed = (Speed_value * 5)/1024;
-  finalSpeed = finalSpeed/0.2;
-  delay(500);
 
-  Speed = (finalSpeed * 3000)/1.7;
- 
-  Serial.println(finalSpeed);
-  Serial.println("Speed is:  ");
-   Serial.print(Speed) ;
 
-   Serial.println(" RPM");
-   delay(500);  
-   Serial.println( );
+int RpmFunction(){
 
-  }
+    float rpm_speed; 
+    float analog_read_speed_value = 0; //initialize speed value as zero
+    float volt_speed;
+    
+  //  for(int i=0; i<150; i++){
+    analog_read_speed_value = analogRead(SpeedSensor); //reads speed value (range between 0 to 1024)
+  // }
+    delay(15); 
+    //Speed_value = Speed_value/150;
   
+    volt_speed = (analog_read_speed_value * 5)/1024; //5volts corresponds to a 1024 measured input
+  // volt_speed = volt_speed/0.2; 
+    delay(500);
+
+    rpm_speed = (volt_speed * 3000)/1.7; //since an rpm of 3000 corresponds to 1.7volts
+    
+    Serial.print("Analog read value : ");
+    Serial.println(analog_read_speed_value);
+
+    Serial.print("volts from RPM meter is :")
+    Serial.println(volt_speed);
+
+    Serial.print("RPM is: ");
+    Serial.print(rpm_speed); 
+
+ return rpm_speed;
+}
+
+int TorqueFunction(){
+  int newton_meter_torque;
+
+
+
+
+
+
+  return newton_meter_torque;
+}
+
